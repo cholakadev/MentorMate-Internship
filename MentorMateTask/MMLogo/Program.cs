5096b2d6835aec7a25a885d99f1b40da997be80e
cholakovge <cholakovge@gmail.com>
2020-02-17 12:05:31 +0200

    Top part done, todo refactoring

--- MentorMateTask/MMLogo/Program.cs
+++ MentorMateTask/MMLogo/Program.cs
@@ -11,17 +11,51 @@ namespace MMLogo
             int logoLength = n * 5;
 
             int logoHeight = n + 1;
+            int logoTopPartHeight = logoHeight / 2;
 
-            for (int i = 0; i < logoHeight / 2; i++)
+            for (int i = 0; i < logoTopPartHeight; i++)
             {
                 for (int j = 0; j < logoLength + 1; j++)
                 {
-                    PrintFirstLine(j, n);
+                    if (i == 0)
+                    {
+                        PrintFirstLine(j, n);
+                    }
 
                     /* Depening on first line, print all the other lines 
-                    by decreasing or increasing the amount of '-'/'*' */
+                     * by decreasing or increasing the amount of '-'/'*' */
+                    else
+                    {
+                        PrintNextLines(j, n, i);
+                    }
                 }
                 Console.WriteLine();
+            }
+        }
+
+        public static void PrintNextLines(int index, int n, int i)
+        {
+            if (index < n - i)
+            {
+                Console.Write('-');
+            }
+            else if (index > n - i && index <= 2 * n + i)
+            {
+                Console.Write('*');
+            }
+            else if (index > 2 * n - i && index <= 3 * n - i)
+            {
+                Console.Write('-');
+            }
+
+            else if (index > 3 * n - i && index <= 4 * n + i)
+            {
+                Console.Write('*');
+            }
+
+            else if (index > 4 * n +i && index <= 5 * n)
+            {
+                Console.Write('-');
             }
         }
 
@@ -51,11 +85,6 @@ namespace MMLogo
             {
                 Console.Write('-');
             }
-        }
-
-        public static void PrintSecondLine()
-        {
-
         }
     }
 }
